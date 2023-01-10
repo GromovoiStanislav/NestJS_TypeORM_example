9bb0e8777ac748618b1665558fd3989fd37ef2a7
Gromovoi Stanislav <gromila12@rambler.ru>
2023-01-10 10:55:09 +0600

    migrations

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## NestJS with TypeORM relations examle
+## NestJS with TypeORM relations, migrations examle
+
+migration:
+
+```
+npm run migration:generate  -- db/migrations/NewMigration
+
+npm run migration:run
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,22 +7,24 @@ import { ContactInfo } from "./contact-info.entity";
 import { Task } from "./task.entity";
 import { Meeting } from "./meeting.entity";
 import { User } from "./user.entity";
-import { Pet } from "./pet.entity";
+import { dataSourceOptions } from '../db/data-source'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "sqlite",
-      database: "db.sqlite",
-      //entities: ['dist/**/*.entity.js'],
-      autoLoadEntities: true,
-      synchronize: true,
-      logging: false
-    }),
-    TypeOrmModule.forFeature([Employee, ContactInfo, Task, Meeting, User, Pet])
+    TypeOrmModule.forRoot(dataSourceOptions),
+    TypeOrmModule.forFeature([Employee, ContactInfo, Task, Meeting, User])
   ],
   controllers: [AppController],
   providers: [AppService]
 })
 export class AppModule {
 }
+
+    // {
+    //   type: "sqlite",
+    //   database: "db.sqlite",
+    //   //entities: ['dist/**/*.entity.js'],
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    //   logging: false
+    // }
